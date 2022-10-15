8c1827b7b1bce6125ef0cbd80d3b2b87d120c7e3
jmatejcz <j.matejczyk@stud.elka.pw.edu.pl>
2022-10-15 16:25:29 +0200

    changing folders layout

---
+++ datasets/PupilCoreDatasetCoords.py
@@ -0,0 +1,31 @@
+import torch
+from torchvision import transforms
+from utils import get_labels_from_csv, get_frames_from_video
+
+
+class PupilCoreDatasetCoords(torch.utils.data.Dataset):
+    def __init__(
+        self, eye0_video_path, eye0_labels_path, eye1_video_path, eye1_labels_path
+    ) -> None:
+        super().__init__()
+        self.eye0_video_path = eye0_video_path
+        self.eye0_labels_path = eye0_labels_path
+        self.eye1_video_path = eye1_video_path
+        self.eye1_labels_path = eye1_labels_path
+        self.eye0_labels_df = get_labels_from_csv(self.eye0_labels_path)
+        self.eye0_frames = get_frames_from_video(self.eye0_video_path)
+        self.eye1_labels_df = get_labels_from_csv(self.eye1_labels_path)
+        self.eye1_frames = get_frames_from_video(self.eye1_video_path)
+
+    def __getitem__(self, idx):
+        pupil_coords = (
+            self.eye0_labels_df.at[idx, "pupil_center_x_coord"],
+            self.eye0_labels_df.at[idx, "pupil_center_y_coord"],
+        )
+        image = self.eye0_frames[idx]
+        T = transforms.Compose([transforms.ToTensor()])
+        image = T(image)
+        return image, pupil_coords
+
+    def __len__(self):
+        return len(self.eye0_frames)
